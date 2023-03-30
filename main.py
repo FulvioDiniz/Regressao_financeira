d1831559a5c23da7de0fc2fb9f0d702686bee0cf
Fulvio <fulviodiniz0@gmail.com>
2023-03-29 22:45:27 -0300

    ver.funcional4.0

--- main.py
+++ main.py
@@ -25,13 +25,18 @@ erros_compra = 0
 acertos_venda = 0
 erros_venda = 0
 cont = 0
+cont2 = 0
 chat_id = last_chat_id(token)
 #mensagem = "Tendência de alta, pode ser uma boa hora para comprar"
 #mensagem2 = "Tendência de baixa, pode ser uma boa hora para vender"
 
 
+# Armazenar o preço anterior
+preco_anterior = None
+
+
 def adicionar_valor(y):
-    global acertos_compra, erros_compra, acertos_venda, erros_venda, ultima_impressao, cont, chat_id, token
+    global acertos_compra, erros_compra, acertos_venda, erros_venda, ultima_impressao, cont, chat_id, token, cont2
     y_vals.append(y)
 
     # Gerar novo valor de x a partir do comprimento da lista de valores de y
@@ -42,52 +47,58 @@ def adicionar_valor(y):
 
     # Calcular previsão para daqui a 10 minutos
     proximo_x = len(y_vals) + (intervalo_previsao / timedelta(minutes=5))
-    previsao = regressor.predict([[proximo_x]])[0] 
-    print(f"Última impressão: {ultima_impressao}")
-    print(f"Valor do y: {y}")
+    #previsao = regressor.predict([[proximo_x]])[0] 
+    #print(f"Última impressão: {ultima_impressao}")
+    #print(f"Valor do y: {y}")
     tempo = datetime.now() - ultima_impressao
     print(f"Tempo desde a última impressão: {tempo}")
-    print(f"proximo_x: {proximo_x}")
+    #print(f"proximo_x: {proximo_x}")
+    cont2 = cont2 + 1
     print(f"valor contador: {cont}")
+    print(f"valor contador2: {cont2}")
+    #print(f"valor y = {y}")
     #send_message(token, chat_id, 'teste')
     
-    #send_message = ('6037164173:AAFWH_Ojc434tGzrkHoZtKIz5FJn_szEOe8',-925570433, 'Tendência de alta, pode ser uma boa hora para comprar')
+    #send_message = (token,-961226810, 'Tendência de alta, pode ser uma boa hora para comprar')
    
 
     # Definir estratégia simples de compra e venda
     if datetime.now() - ultima_impressao >= intervalo_previsao:
         previsao2 = regressor.predict([[proximo_x]])[0]
         print(f"Previsão para daqui a 10 minutos: {previsao2}")
-        cont = cont + 1     
+        y2 = y_vals[-cont2]
+        y1 = y_vals[cont2]
+        #cont = cont + 1     
         #print(f"Valor da previsão: {previsao}")
-        if cont > 0:
-            if  regressor.coef_ > 0:
-                print("Tendência de alta, pode ser uma boa hora para comprar")
-                send_message(token, chat_id, 'Lembrando (moeda: EUR/USD) Previsões  de 10 minutos Tendência de alta, pode ser uma boa hora para comprar')
+        if  regressor.coef_ > 0:
+            print("Tendência de alta, pode ser uma boa hora para comprar")
+            send_message(token, chat_id, 'Lembrando (moeda: EUR/USD) Previsões  de 10 minutos Tendência de alta, pode ser uma boa hora para comprar')
                 #send_message = ('6037164173:AAFWH_Ojc434tGzrkHoZtKIz5FJn_szEOe8',-925570433, 'Tendência de alta, pode ser uma boa hora para comprar')
-                if y < previsao2:
-                    acertos_compra += 1
-                    print("Acerto!")
-                else:
-                    erros_compra += 1
-                    print("Erro!")
+            if y2 < y1:
+                acertos_compra += 1
+                send_message(token, chat_id, 'Acertou a tendencia de alta')
             else:
-                print("Tendência de baixa, pode ser melhor vender")
-                send_message(token, chat_id, 'Lembrando (moeda: EUR/USD) Previsões  de 10 minutos : Tendência de Baixa, pode ser uma boa hora para vender')
+                erros_compra += 1
+                send_message(token, chat_id, 'Errou a tendencia de alta')
+        else:
+            print("Tendência de baixa, pode ser melhor vender")
+            send_message(token, chat_id, 'Lembrando (moeda: EUR/USD) Previsões  de 10 minutos : Tendência de Baixa, pode ser uma boa hora para vender')
                 #send_message = ('6037164173:AAFWH_Ojc434tGzrkHoZtKIz5FJn_szEOe8',-925570433, 'Tendência de Baixa, pode ser uma boa hora para comprar')
-                if y > previsao2:
-                    acertos_venda += 1
-                    print("Acerto!")
-                else:
-                    erros_venda += 1
-                    print("Erro!")
+            if y2 > y1:
+                acertos_venda += 1
+                send_message(token, chat_id, 'Acertou a tendencia de alta')
+            else:
+                erros_venda += 1
+                send_message(token, chat_id, 'Errou a tendencia de baixa')
 
         ultima_impressao = datetime.now()
+        cont = cont + 1
 
         # Calcular taxa de acerto
-        taxa_acerto_compra = (acertos_compra + acertos_venda)/cont * 100
-        print(f"Taxa de acerto para compra: {taxa_acerto_compra}%")
-        send_message(token, chat_id, f"Taxa de acerto (Em teste, não verdadeira): {taxa_acerto_compra}%")
+        if cont > 0:
+            taxa_acerto_compra = (acertos_compra + acertos_venda)/cont * 100
+            print(f"Taxa de acerto para compra: {taxa_acerto_compra}%")
+            send_message(token, chat_id, f"Taxa de acerto (Em teste, não verdadeira): {taxa_acerto_compra}%")
 
 def on_message(ws, message):
     data = json.loads(message)
@@ -122,6 +133,7 @@ if __name__ == "__main__":
 
         # Atualizar a variável de próxima atualização
         proxima_atualizacao += intervalo_previsao
+        #cont2 = cont2 + 1
 
         # Gerar nova previsão e calcular taxa de acerto
         #adicionar_valor(0)
